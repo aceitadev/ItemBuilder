9058b61c3b788fcda450987b27c5c9006cfce382
aceita <155707217+aceitadev@users.noreply.github.com>
2024-11-01 15:34:22 -0300

    Update ItemBuilder.java

--- ItemBuilder.java
+++ ItemBuilder.java
@@ -1,4 +1,3 @@
-import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -6,30 +5,36 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
 public class ItemBuilder {
-    
+
     private final ItemStack item;
 
     public ItemBuilder(Material material) {
         item = new ItemStack(material);
     }
-    
+
+    public ItemStack build() {
+        return item;
+    }
+
     public void setAmount(int amount) {
         item.setAmount(amount);
     }
+
     public void setName(String name) {
         name = name.replace("&", "§");
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(name);
         item.setItemMeta(meta);
     }
+
     public void setLore(List<String> lore) {
         lore = lore.stream().map(line -> line.replace("&", "§")).toList();
         ItemMeta meta = item.getItemMeta();
         meta.setLore(lore);
         item.setItemMeta(meta);
     }
+
     public void setLore(String lore) {
         lore = lore.replace("&", "§");
         List<String> loreList = new ArrayList<>();
@@ -38,6 +43,7 @@ public class ItemBuilder {
         meta.setLore(loreList);
         item.setItemMeta(meta);
     }
+
     public void setLore(String... lore) {
         List<String> loreList = new ArrayList<>();
         for (String line : lore) {
@@ -56,11 +62,11 @@ public class ItemBuilder {
         meta.setLore(lore);
         item.setItemMeta(meta);
     }
-    
+
     public void setModelID(int modelId) {
         ItemMeta meta = item.getItemMeta();
         meta.setCustomModelData(modelId);
         item.setItemMeta(meta);
     }
-    
+
 }
