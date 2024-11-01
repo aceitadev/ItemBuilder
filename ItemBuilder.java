62319fdccadf9e8d41ea6e06c78651757141a314
aceita <155707217+aceitadev@users.noreply.github.com>
2024-11-01 15:31:12 -0300

    Update ItemBuilder.java

--- ItemBuilder.java
+++ ItemBuilder.java
@@ -1,3 +1,4 @@
+import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -5,31 +6,61 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class ItemBuilder {
+    
+    private final ItemStack item;
 
-    public static ItemStack build(Material material) {
-        return new ItemStack(material);
+    public ItemBuilder(Material material) {
+        item = new ItemStack(material);
     }
-
-    public static ItemStack build(Material material, String name) {
-        ItemStack item = new ItemStack(material);
-        ItemMeta meta = item.getItemMeta();
+    
+    public void setAmount(int amount) {
+        item.setAmount(amount);
+    }
+    public void setName(String name) {
         name = name.replace("&", "§");
+        ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(name);
         item.setItemMeta(meta);
-        return item;
+    }
+    public void setLore(List<String> lore) {
+        lore = lore.stream().map(line -> line.replace("&", "§")).toList();
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+    }
+    public void setLore(String lore) {
+        lore = lore.replace("&", "§");
+        List<String> loreList = new ArrayList<>();
+        loreList.add(lore);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(loreList);
+        item.setItemMeta(meta);
+    }
+    public void setLore(String... lore) {
+        List<String> loreList = new ArrayList<>();
+        for (String line : lore) {
+            loreList.add(line.replace("&", "§"));
+        }
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(loreList);
+        item.setItemMeta(meta);
     }
 
-    public static ItemStack build(Material material, String name, List<String> lore) {
-        ItemStack item = new ItemStack(material);
+    public void addLineLore(String line) {
+        line = line.replace("&", "§");
+        List<String> lore = item.getItemMeta().getLore();
+        lore.add(line);
         ItemMeta meta = item.getItemMeta();
-        name = name.replace("&", "§");
-        meta.setDisplayName(name);
-        List<String> mutableLore = new ArrayList<>(lore);
-        mutableLore.replaceAll(s -> s.replace("&", "§"));
-        meta.setLore(mutableLore);
+        meta.setLore(lore);
         item.setItemMeta(meta);
-        return item;
+    }
+    
+    public void setModelID(int modelId) {
+        ItemMeta meta = item.getItemMeta();
+        meta.setCustomModelData(modelId);
+        item.setItemMeta(meta);
     }
     
 }
