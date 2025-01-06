b49e65fa983d06f6850138c48669b121e823757d
aceita <155707217+aceitadev@users.noreply.github.com>
2025-01-06 02:40:24 -0300

    Update ItemBuilder.java

--- ItemBuilder.java
+++ ItemBuilder.java
@@ -1,14 +1,24 @@
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ItemBuilder {
 
-    private final ItemStack item;
+    private ItemStack item;
+
+    public ItemBuilder() {
+    }
 
     public ItemBuilder(Material material) {
         item = new ItemStack(material);
@@ -22,6 +32,42 @@ public class ItemBuilder {
         item.setAmount(amount);
     }
 
+    public void setHead(UUID uuid) {
+        ItemStack playerHead = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        SkullMeta skullMeta = (SkullMeta) playerHead.getItemMeta();
+        if (skullMeta != null) {
+            skullMeta.setOwner(Bukkit.getOfflinePlayer(uuid).getName());
+            playerHead.setItemMeta(skullMeta);
+        }
+        item = playerHead;
+    }
+    public void setHead(String base64) {
+        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+        if (skullMeta != null) {
+            try {
+                GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+                profile.getProperties().put("textures", new Property("textures", base64));
+                Field profileField = skullMeta.getClass().getDeclaredField("profile");
+                profileField.setAccessible(true);
+                profileField.set(skullMeta, profile);
+
+                skull.setItemMeta(skullMeta);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        item = skull;
+    }
+
+    public void setMaterial(Material material) {
+        if (item != null) {
+            item.setType(material);
+        } else {
+            item = new ItemStack(material);
+        }
+    }
+
     public void setName(String name) {
         if (name == null) return;
         name = name.replace("&", "§");
@@ -32,12 +78,9 @@ public class ItemBuilder {
 
     public void setLore(List<String> lore) {
         if (lore == null) return;
-        List<String> loreList = new ArrayList<>();
-        for (String line : lore) {
-            loreList.add(line.replace("&", "§"));
-        }
+        lore = lore.stream().map(s -> s.replace("&", "§")).collect(Collectors.toList());
         ItemMeta meta = item.getItemMeta();
-        meta.setLore(loreList);
+        meta.setLore(lore);
         item.setItemMeta(meta);
     }
 
@@ -69,12 +112,6 @@ public class ItemBuilder {
         lore.add(line);
         ItemMeta meta = item.getItemMeta();
         meta.setLore(lore);
-        item.setItemMeta(meta);
-    }
-
-    public void setModelID(int modelId) {
-        ItemMeta meta = item.getItemMeta();
-        meta.setCustomModelData(modelId);
         item.setItemMeta(meta);
     }
 
